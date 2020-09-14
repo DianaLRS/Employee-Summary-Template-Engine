6ae86ac50d9e97e0875b22e1ed8603115c6ccf80
Luna <dlramos194@gmail.com>
2020-09-14 18:52:44 -0500

    employee class

--- Develop/app.js
+++ Develop/app.js
@@ -1,15 +1,53 @@
+const Employee = require("./lib/Employee")
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+const Team = []
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 
+
+
+
+function createManager() {
+    return inquirer.prompt([{
+            type: "input",
+            name: "ManagerName",
+            message: "What is your manager's name?"
+        },
+        {
+            type: "input",
+            name: "managerIDnumber",
+            message: "What is your manager's ID number?"
+        },
+
+        {
+            type: "input",
+            name: "ManagerEmail",
+            message: "What is your manager's email?"
+        },
+        {
+            type: "input",
+            name: "ManagerOfficeNumber",
+            message: "What is your manager's office number?"
+        },
+    ])
+}
+
+
+function createEmployee()
+
+function createEngineer()
+
+function createIntern()
+
+
+console.log("Enter the manager's info")
+createManager();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
